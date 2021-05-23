42b9d01b12f783f4a14564a53b54d4e84def2e1b
Irina Stepanova <irina.s.vesna@mail.ru>
2021-05-23 20:25:55 +0600

    feat: add dotenv, db constants

--- db.js
+++ db.js
@@ -1,7 +1,8 @@
+require('dotenv').config();
 const Sequelize = require('sequelize');
                                 //database username   password
-const sequelize = new Sequelize('gamedb', 'postgres', 'ghastb0i', {
-    host: 'localhost',
+const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASS, {
+    host: process.env.DB_HOST,
     dialect: 'postgres'
 })
 
