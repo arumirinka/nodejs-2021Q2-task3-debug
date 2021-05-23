9047638933288c2a25464b85c45ac8c13e37ffe1
Irina Stepanova <irina.s.vesna@mail.ru>
2021-05-24 02:15:34 +0600

    fix: gamecontroller

--- controllers/gamecontroller.js
+++ controllers/gamecontroller.js
@@ -1,10 +1,10 @@
 var router = require('express').Router();
-var Game = require('../models/game');
+var Game = require('../db').import('../models/game');
 
 router.get('/all', (req, res) => {
     Game.findAll({ where: { owner_id: req.user.id } })
         .then(
-            function findSuccess(data) {
+            function findSuccess(games) {
                 res.status(200).json({
                     games: games,
                     message: "Data fetched."
@@ -70,7 +70,7 @@ router.put('/update/:id', (req, res) => {
         {
             where: {
                 id: req.params.id,
-                owner_id: req.user
+                owner_id: req.user.id
             }
         })
         .then(
