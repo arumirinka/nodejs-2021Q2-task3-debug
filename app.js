775d8444803f46c0125c3c59010edfb95b08dc3f
Irina Stepanova <irina.s.vesna@mail.ru>
2021-05-24 01:49:02 +0600

    fix: app body-parser

--- app.js
+++ app.js
@@ -4,9 +4,8 @@ var db = require('./db');
 var user = require('./controllers/usercontroller');
 var game = require('./controllers/gamecontroller')
 
-
 db.sync();
-app.use(require('body-parser'));
+app.use(express.json());
 app.use('/api/auth', user);
 app.use(require('./middleware/validate-session'))
 app.use('/api/game', game);
